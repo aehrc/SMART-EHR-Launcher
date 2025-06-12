ba2c5bf3add3ac0174229da9700f576b9f5b6df7
Sean Fong <sean.fong@csiro.au>
2025-06-12 12:57:26 +0930

    Update localhost:5173 client ID in preconfigured apps

--- src/utils/appLaunch.ts
+++ src/utils/appLaunch.ts
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2025 Commonwealth Scientific and Industrial Research
+ * Organisation (CSIRO) ABN 41 687 119 230.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // List of SMART apps should be registered in your server, otherwise you will get an error when launching
 export const preconfiguredApps = [
   {
@@ -60,7 +77,7 @@ export const preconfiguredApps = [
   {
     app_name: "localhost:5173",
     launch_url: "http://localhost:5173/launch",
-    client_id: "1ff7bdc2-36b2-4303-8c05-c57342c5b043",
+    client_id: "a57d90e3-5f69-4b92-aa2e-2992180863c1",
     scope:
       "fhirUser online_access openid profile patient/Condition.rs patient/Observation.rs launch patient/Encounter.rs patient/QuestionnaireResponse.cruds patient/Patient.rs",
     redirect_uris: "http://localhost:5173/",
